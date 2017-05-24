84f6f97cbd492496044da4d88ff9e0b54b86fd24
Paul Sarrassat <poltron54@gmail.com>
2017-05-24 11:10:36 +0200

    AddWithApiManager fonctionnel. Tests sur les Adapter dans addAction.

--- src/TestM/Controller/ListController.php
+++ src/TestM/Controller/ListController.php
@@ -5,18 +5,26 @@ namespace TestM\Controller;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 use Omeka\Api\Representation\ItemRepresentation;
+use Omeka\Api\Representation\ResourceClassRepresentation;
+use Omeka\Api\Representation\ResourceTemplateRepresentation;
 use Omeka\Entity\Item;
+use Omeka\Entity\Property;
+use Omeka\Entity\Value;
 //use TestM\Job\Insert;
 //Job\Insert;
 
 class ListController extends AbstractActionController {
     
     protected $config; //utilité ?
-    protected $api;
+    /* @var $api \Omeka\Api\Manager */
+	protected $api;
+	/* @var $adapterManager \Omeka\Api\Adapter\Manager */
+    protected $adapterManager;
     
-    public function __construct(array $config, $api) {
+    public function __construct(array $config, $api, $adapterManager) {
         $this->config = $config;
-        $this->api = $api;
+		$this->api = $api;
+		$this->adapterManager = $adapterManager;
     }
     
     /**
@@ -27,19 +35,80 @@ class ListController extends AbstractActionController {
         // Création de l'entité
         $item = new Item();
         $item->setIsPublic(true);
-        $item->setCreated(new DateTime('now'));
-        $item->setModified(new DateTime('now'));
+        $item->setCreated(new \DateTime('now'));
+        $item->setModified(new \DateTime('now'));
 		
+		/* @var $resourceClass \Omeka\Entity\ResourceClass */
+		$resourceClass=$this->adapterManager->get('resource_classes')
+				->findEntity(['localName'=>'Location']);
+		$item->setResourceClass($resourceClass);
+		
+		/* @var $resourceTemplate \Omeka\Entity\ResourceTemplate */
+		$resourceTemplate=$this->adapterManager->get('resource_templates')
+				->findEntity(['label'=>'Lieu d’archives']);
+		$item->setResourceTemplate($resourceTemplate);
+		
+		/* @var $properties \Doctrine\Common\Collections\ArrayCollection */
+		$properties = $item->getValues(); //RDF triples
+		
+		$archiveProperties=[
+			'dcterms'=>["title"],
+			'locn'=>["address"],
+			'foaf'=>['homepage'],
+		];
+		$archiveAdresseProperties=[
+			'locn'=>['fullAddress','postName','adminUnitL1'],
+		];
+		
+		//For each vocabulary	
+			//get the vocabulary
+			/* @var $vocabularyAdapter \Omeka\Api\Adapter\VocabularyAdapter */
+			$vocabularyAdapter = $this->adapterManager->get('vocabularies');
+			/* @var $vocabulary \Omeka\Entity\Vocabulary */
+			$vocabulary=$vocabularyAdapter->findEntity(['prefix'=>'bibo']);
+
+			//For each property
+				//get the property from the vocabulary or maybe we must create one
+	//			$property = new Property();
+	//			$property->setVocabulary($vocabulary);
+			
+				/* @var $terms \Doctrine\Common\Collections\ArrayCollection */
+				$terms=$vocabulary->getProperties();
+				
+				//Voir comment on peut récupérer la propriété voulue, est-ce
+				// que les termes sont les clés dans cette ArrayCollection ?
+				
+//				/* @var $terms array[PropertyRepresentation] */
+//				$terms=$vocabularyAdapter->getRepresentation($vocabulary)->properties();
+				
+				//Create a value and set value properties
+				$value= new Value();
+				$value->setValue("ma valeur"); //or other setter
+
+				//Link property to the value
+				$value->setProperty($property);
+
+				//Add the property-linked value to the item
+				$properties->add($value);
+		
+		//On crée une representation de l’item
+		$itemAdapter = $this->adapterManager->get('items');
+		/* @var $itemRpz ItemRepresentation */
+		$itemRpz = $itemAdapter->getRepresentation($item);
+		echo json_encode($itemRpz);
+		
+		
+		/* @var $em \Doctrine\ORM\EntityManager */
+//		$em;
         // On récupère l'EntityManager
 //        $em = $this->getEvent()->getApplication()->getServiceManager()
 //                ->get('doctrine.orm.entity_manager');
-        //Contenu dans $this->doctrine
 
         // Étape 1 : On « persiste » l'entité
-        $em->persist($item);
+//        $em->persist($item);
 
         // Étape 2 : On « flush » tout ce qui a été persisté avant
-        $em->flush();
+//        $em->flush();
 
         // Reste de la méthode qu'on avait déjà écrit
 //        if ($request->isMethod('POST')) {
@@ -52,7 +121,44 @@ class ListController extends AbstractActionController {
     
 
 	/**
-     * Get an item using ApiManager().
+     * Insert an item using ApiManager.
+     */
+    private function addWithApiManager() {
+		$data=[
+			"o:resource_class"=>["o:id"=>40],
+			"title"=> [[
+				"type"=> "literal",
+				"property_id"=> 1,
+				"@value"=> "Tout en un"
+			]],
+			"desc"=> [[
+				"type"=> "literal",
+				"property_id"=> 4,
+				"@value"=> "Un super les livre sur les savants et les écrivains"
+			]],
+			"createur"=> [[
+				"type"=> "literal",
+				"property_id"=> 2,
+				"@value"=> "C’est bien lui, c’est Henri !"
+			],[
+				"type"=> "resource",
+				"property_id"=> 5,
+				"value_resource_id"=> 6,
+			],[
+				"type"=> "resource",
+				"property_id"=> 2,
+				"value_resource_id"=> 6,
+			],[
+				"type"=> "uri",
+				"property_id"=> 3,
+				"@id"=> "https:\/\/google.com",
+				"o:label"=> "Moteur de recherche Google"
+			]],
+		];
+		return $this->api->create('items', $data)->getContent();
+	}
+	/**
+     * Get an item using ApiManager.
      * @param int $idItem
      */
     private function affWithApiManager(int $idItem) {
@@ -73,19 +179,17 @@ class ListController extends AbstractActionController {
     }
     public function affAction() {
         
-        //Display an item using API
-        //$this->aff(1);
-        
         //Insert an item using API
         //$item=json_decode(file_get_contents(__DIR__ . '/../../../assets/item.json'), true);
         //$this->insert($item);
         
         //Display an item using EntityManager
-        
-		$content = $this->affWithApiManager(4);
-        
-        return new ViewModel([
-            'content' => $content
-        ]);
+//		$content = $this->affWithApiManager(32);
+		
+		$content=json_encode($this->addWithApiManager());
+		
+		return new ViewModel([
+			'content' => $content
+		]);
     }
 }
--- src/TestM/Factory/ListControllerFactory.php
+++ src/TestM/Factory/ListControllerFactory.php
@@ -13,10 +13,12 @@ class ListControllerFactory implements FactoryInterface{
     
     public function __invoke(ContainerInterface $serviceLocator, $requestedName, array $options = null) {
         //$mediaIngesterManager = $serviceLocator->get('Omeka\MediaIngesterManager');
-        $doctrine = $serviceLocator->get('Omeka\ApiManager');
+        $apiManager = $serviceLocator->get('Omeka\ApiManager');
         $config = $serviceLocator->get('Config');
         
-        return new ListController($config, $doctrine);
+		$adapterManager= $serviceLocator->get('Omeka\ApiAdapterManager');
+		
+        return new ListController($config, $apiManager, $adapterManager);
     }
 
 }
